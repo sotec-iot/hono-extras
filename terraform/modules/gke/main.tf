b48c1f61e8374adf1b22073cfef715900c1a7f54
Elias Ebi <e.ebi@sotec.eu>
2024-10-04 11:19:53 +0200

    add different cluster deployments depending on input flag

--- terraform/modules/gke/main.tf
+++ terraform/modules/gke/main.tf
@@ -42,7 +42,13 @@ resource "google_container_cluster" "hono_cluster" {
   }
 }
 
+
+
+##################################
+######## Autopilot-cluster ########
+##################################
 resource "google_service_account_iam_binding" "workload_identity_binding" {
+  count = var.gke_enable_autopilot ? 1 : 0
   members            = [
     "serviceAccount:sotec-iot-core-dev.svc.id.goog[hono/default]",
     "serviceAccount:sotec-iot-core-dev.svc.id.goog[hono/eclipse-hono-adapter]",
@@ -52,22 +58,24 @@ resource "google_service_account_iam_binding" "workload_identity_binding" {
   service_account_id =  "projects/${var.project_id}/serviceAccounts/${var.gke_service_account_email}"
 }
 
-#resource "kubernetes_annotations" "gke_gcp_binding" {
-#  api_version = "v1"
-#  kind        = "ServiceAccount"
-#
-#  metadata {
-#    name = "default"
-#    namespace = "hono"
-#  }
-#
-#  annotations = {
-#    "iam.gke.io/gcp-service-account" = var.gke_service_account_email
-#  }
-#}
+resource "kubernetes_annotations" "gke_gcp_binding" {
+  count = var.gke_enable_autopilot ? 1 : 0
+  api_version = "v1"
+  kind        = "ServiceAccount"
 
-#
+  metadata {
+    name = "default"
+    namespace = "hono"
+  }
+
+  annotations = {
+    "iam.gke.io/gcp-service-account" = var.gke_service_account_email
+  }
+}
+
+
 #resource "google_container_cluster" "hono_cluster" {
+#  count = var.gke_enable_autopilot ? 1 : 0
 #  name                     = var.gke_cluster_name
 #  project                  = var.project_id
 #  location                 = var.region
@@ -99,51 +107,56 @@ resource "google_service_account_iam_binding" "workload_identity_binding" {
 #    }
 #  }
 #}
-#
-#resource "google_container_node_pool" "standard_node_pool" {
-#  name               = var.gke_node_pool_name
-#  project            = var.project_id
-#  location           = var.region
-#  cluster            = google_container_cluster.hono_cluster.name
-#  initial_node_count = var.node_pool_initial_node_count
-#  node_locations     = var.node_locations
-#  management {
-#    auto_repair  = true
-#    auto_upgrade = true
-#  }
-#  dynamic "autoscaling" {
-#    for_each = var.node_pool_autoscaling_enabled ? [1] : []
-#    content {
-#      min_node_count = var.node_pool_min_node_count
-#      max_node_count = var.node_pool_max_node_count
-#    }
-#  }
-#  node_config {
-#    machine_type    = var.gke_machine_type
-#    local_ssd_count = 0
-#    disk_size_gb    = var.node_pool_disk_size
-#    disk_type       = var.node_pool_disk_type
-#    image_type      = "COS_CONTAINERD"
-#    preemptible     = false
-#
-#    service_account = var.gke_service_account_email
-#    oauth_scopes    = [
-#      "https://www.googleapis.com/auth/cloud-platform"
-#    ]
-#  }
-#  upgrade_settings {
-#    strategy        = var.node_pool_upgrade_strategy
-#    max_surge       = var.node_pool_upgrade_strategy == "SURGE"? var.node_pool_max_surge : null
-#    max_unavailable = var.node_pool_upgrade_strategy == "SURGE"? var.node_pool_max_unavailable : null
-#    dynamic "blue_green_settings" {
-#      for_each = var.node_pool_upgrade_strategy != "SURGE" ? [1] : []
-#      content {
-#        standard_rollout_policy {
-#          batch_node_count    = var.node_pool_batch_node_count
-#          batch_soak_duration = var.node_pool_batch_soak_duration
-#        }
-#        node_pool_soak_duration = var.node_pool_soak_duration
-#      }
-#    }
-#  }
-#}
+
+##################################
+######## Standard-cluster ########
+##################################
+
+resource "google_container_node_pool" "standard_node_pool" {
+  count = var.gke_enable_autopilot ? 0 : 1
+  name               = var.gke_node_pool_name
+  project            = var.project_id
+  location           = var.region
+  cluster            = google_container_cluster.hono_cluster.name
+  initial_node_count = var.node_pool_initial_node_count
+  node_locations     = var.node_locations
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+  dynamic "autoscaling" {
+    for_each = var.node_pool_autoscaling_enabled ? [1] : []
+    content {
+      min_node_count = var.node_pool_min_node_count
+      max_node_count = var.node_pool_max_node_count
+    }
+  }
+  node_config {
+    machine_type    = var.gke_machine_type
+    local_ssd_count = 0
+    disk_size_gb    = var.node_pool_disk_size
+    disk_type       = var.node_pool_disk_type
+    image_type      = "COS_CONTAINERD"
+    preemptible     = false
+
+    service_account = var.gke_service_account_email
+    oauth_scopes    = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+  upgrade_settings {
+    strategy        = var.node_pool_upgrade_strategy
+    max_surge       = var.node_pool_upgrade_strategy == "SURGE"? var.node_pool_max_surge : null
+    max_unavailable = var.node_pool_upgrade_strategy == "SURGE"? var.node_pool_max_unavailable : null
+    dynamic "blue_green_settings" {
+      for_each = var.node_pool_upgrade_strategy != "SURGE" ? [1] : []
+      content {
+        standard_rollout_policy {
+          batch_node_count    = var.node_pool_batch_node_count
+          batch_soak_duration = var.node_pool_batch_soak_duration
+        }
+        node_pool_soak_duration = var.node_pool_soak_duration
+      }
+    }
+  }
+}
